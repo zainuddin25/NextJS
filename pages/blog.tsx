89f753f040439828b80312777b9a03f2036ed646
zainuddin25 <zanukjepara@gmail.com>
2022-06-15 13:45:26 +0700

    Latihan EPS 1 - EPS 14

---
+++ pages/blog.tsx
@@ -0,0 +1,5 @@
+export default function Blog() {
+    return (
+        <p>Blog Menu</p>
+    )
+}
---
+++ pages/user/index.tsx
@@ -0,0 +1,5 @@
+export default function dataList() {
+    return (
+        <p>User Page</p>
+    )
+}
